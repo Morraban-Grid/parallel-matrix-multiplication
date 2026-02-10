b598c262150ee5c8a90994c085463812a15fff7c
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-02-10 08:39:06 -0500

    Add SIMD header for future vectorization optimizations

---
+++ include/simd.h
@@ -0,0 +1,25 @@
+#ifndef SIMD_H
+#define SIMD_H
+
+/*
+ * File: simd.h
+ * Project: Parallel Matrix Multiplication
+ * Description:
+ * Definitions and utilities for SIMD optimization (SSE / AVX).
+ *
+ * NOTE:
+ * This file will be used in later phases of the project
+ * when vectorization techniques are implemented.
+ */
+
+#include <immintrin.h>
+
+/*
+ * Future SIMD macros and functions will be defined here.
+ * Examples:
+ *  - Vector loads
+ *  - Fused multiply-add operations
+ *  - Memory alignment handling
+ */
+
+#endif /* SIMD_H */
