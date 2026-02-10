e0886b4944726dfdb317a80f3108b7d8b80a7c56
Morraban-Grid <andromeda.4i.1000@gmail.com>
2026-02-09 22:49:38 -0500

    Add common header for shared constants and definitions

---
+++ include/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+/* 
+ * File: common.h
+ * Project: Parallel Matrix Multiplication
+ * Description:
+ * Common definitions and constants shared by all implementations
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Size of the square matrix */
+#define MAX 500
+
+#endif /* COMMON_H */
